aea75ac1db643e975f4903c64fb3e71e17fff3e4
Gaurav Chaudhary <42979648+gauravcy19@users.noreply.github.com>
2022-12-31 01:01:10 +0530

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Gaurav Chaudhary on 31/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
